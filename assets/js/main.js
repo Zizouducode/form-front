f98ce961c74ab6a19596c7baecc963cabf4a4359
Karim Hamga <karimhamga@macbook-pro-de-karim-2.home>
2022-10-28 14:53:15 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+//Create script to get form data
+
+//Add Listener on DOC
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  //   Disable auto refresh on form submission
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      console.log("OK");
+
+      //Get data from form and create object to send to back-end
+      const data = {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+      };
+      console.log(data);
+
+      //Send data
+      const response = await axios.post("http://localhost:3000/form", data);
+      console.log(response);
+    });
+});
